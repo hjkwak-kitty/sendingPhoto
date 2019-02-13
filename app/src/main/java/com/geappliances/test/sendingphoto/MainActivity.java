2f124a0c228cf0d67a5fae149313cb74341ce3c7
Kitty Hyojin <Hyojin.Kwak@geappliances.com>
2019-02-13 10:28:22 +0900

    Init Project, Add function to get photo from camera and album

---
+++ app/src/main/java/com/geappliances/test/sendingphoto/MainActivity.java
@@ -0,0 +1,246 @@
+package com.geappliances.test.sendingphoto;
+
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "메인";
+
+    private Button btnSelect;
+    private Button btnCamera;
+    private ImageView imageView;
+
+    private static final int REQUEST_SELECT_PHOTO = 1;
+    private static final int REQUEST_TAKE_PHOTO = 0;
+
+    private Uri imgUri, photoURI, albumURI;
+    private String mCurrentPhotoPath;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        Permission Check
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .check();
+
+
+//        Button Set
+        btnSelect = (Button) findViewById(R.id.btn_select);
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "사진선택클릭");
+                selectAlbum();
+
+            }
+        });
+
+
+        btnCamera = (Button) findViewById(R.id.btn_camera);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "사진찍기클릭");
+                takePhoto();
+            }
+        });
+
+        imageView = (ImageView) findViewById(R.id.image);
+    }
+
+    public void selectAlbum() {
+        Log.d(TAG, "사진선택");
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_SELECT_PHOTO);
+
+    }
+
+
+    public void takePhoto() {
+
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+
+                File photoFile = null;
+
+                try {
+
+                    photoFile = createImageFile();
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+
+                }
+
+                if (photoFile != null) {
+
+                    Uri providerURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+
+                    imgUri = providerURI;
+
+                    intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, providerURI);
+
+                    startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+
+                }
+
+            }
+
+        } else {
+
+            return;
+
+        }
+
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (resultCode != RESULT_OK) {
+
+            return;
+
+        }
+
+        switch (requestCode) {
+            case REQUEST_SELECT_PHOTO: {
+
+                //앨범에서 가져오기
+
+                if (data.getData() != null) {
+
+                    try {
+
+                        File albumFile = null;
+
+                        albumFile = createImageFile();
+
+                        photoURI = data.getData();
+                        albumURI = Uri.fromFile(albumFile);
+                        imageView.setImageURI(photoURI);
+
+                        sendPhoto(photoURI);
+                        //cropImage();
+
+                    } catch (Exception e) {
+
+                        e.printStackTrace();
+
+                        Log.v("알림", "앨범에서 가져오기 에러");
+
+                    }
+
+                }
+
+                break;
+
+            }
+
+            case REQUEST_TAKE_PHOTO: {
+
+                //촬영
+
+                try {
+
+                    Log.v("알림", "FROM_CAMERA 처리");
+
+                    imageView.setImageURI(imgUri);
+                    sendPhoto(imgUri);
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+
+                }
+
+                break;
+
+            }
+
+
+        }
+
+    }
+
+
+    private void sendPhoto(Uri imgUri){
+
+
+
+    }
+
+    PermissionListener permissionlistener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPermissionDenied(List<String> deniedPermissions) {
+            Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+    };
+
+}
